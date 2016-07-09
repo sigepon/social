f3813e1403956324015fcc2ab3fd6e5b70a02bd9
Kikuchi Shigeo <K@sigepon.local>
2016-07-09 13:47:18 +0900

    CRUD style 基本OK

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,25 @@
 class PostsController < ApplicationController
   def index
+    # @posts = Post.all
+    @posts = Post.order(created_at: :desc)
+    @post = Post.new
+  end
+
+  def create
+      @post = Post.new(post_params)
+      @post.save
+      redirect_to root_path
+  end
+
+  def destroy
+      @post = Post.find(params[:id])
+      @post.destroy
+      redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+      params.require(:post).permit(:body, :picture)
   end
 end
